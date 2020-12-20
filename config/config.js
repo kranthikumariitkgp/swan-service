f6344b13f887e8e0752df12183b207ab55ad538f
kranthi.teddu <kranthi.teddu@decurtis.com>
2020-12-20 13:09:14 +0530

    code changes

--- config/config.js
+++ config/config.js
@@ -3,12 +3,12 @@ const port = 3000;
 
 const mongo = {
     "baseURL": "mongodb://localhost:27017/",
-        "dbName": "swan-service",
-        "debug": "true",
-        "username": "swanUsername",
-        "password": "Test@123",
-        "poolSize": 10,
-        "authSource": "swan-service"
+    "dbName": "swan-service",
+    "debug": "true",
+    "username": "swanUsername",
+    "password": "Test@123",
+    "poolSize": 10,
+    "authSource": "swan-service"
 };
 
 module.exports =
